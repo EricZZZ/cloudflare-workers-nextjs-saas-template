814a59a2acf07b2883db05ba912cc96b966ec801
EricZzz <biggogten@gmail.com>
2025-08-01 00:49:30 +0800

    fix(docs): 添加博客使用指南文档支持

---
+++ .source/index.ts
@@ -0,0 +1,7 @@
+// @ts-nocheck -- skip type checking
+import * as docs_2 from "../content/blog/zh/blog-usage-guide.mdx?collection=docs&hash=1753939661985"
+import * as docs_1 from "../content/blog/ja/blog-usage-guide.mdx?collection=docs&hash=1753939661985"
+import * as docs_0 from "../content/blog/en/blog-usage-guide.mdx?collection=docs&hash=1753939661985"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"en/blog-usage-guide.mdx","absolutePath":"/Users/zhaoyang/Documents/git-repositories/cloudflare-workers-nextjs-saas-template/content/blog/en/blog-usage-guide.mdx"}, data: docs_0 }, { info: {"path":"ja/blog-usage-guide.mdx","absolutePath":"/Users/zhaoyang/Documents/git-repositories/cloudflare-workers-nextjs-saas-template/content/blog/ja/blog-usage-guide.mdx"}, data: docs_1 }, { info: {"path":"zh/blog-usage-guide.mdx","absolutePath":"/Users/zhaoyang/Documents/git-repositories/cloudflare-workers-nextjs-saas-template/content/blog/zh/blog-usage-guide.mdx"}, data: docs_2 }], [])
